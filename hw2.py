5de3caac63223f30f0bc432f7877feb285f1b03d
henriquedaponte <henriquedaponte@gmail.com>
2023-10-16 10:12:32 -0700

    created function to calculate the mean squared error of our models prediction

--- hw2.py
+++ hw2.py
@@ -1,3 +1,10 @@
 import numpy as np
 import pandas as pd
 import cvxpy as cp
+
+def meanSquaredError(predicted, actual):
+    ''''
+    Function to calculate the mean squared error
+    '''
+    return np.sum((actual - predicted)**2)/len(predicted)
+
